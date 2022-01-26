776531ab29203a7a91944ac4337a5fedf55a3703
Sergey Belokopytnyy <ser.oks.white@gmail.com>
2022-01-27 02:10:32 +0500

    homework solution 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+function createdGalery(galleryItems) {
+  return galleryItems
+    .map(({ description, original, preview }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" title="${description}"/>
+</a>`;
+    })
+    .join("");
+}
+
+galleryEl.insertAdjacentHTML("beforeend", createdGalery(galleryItems));
+
+var lightbox = new SimpleLightbox(".gallery a", {});
+lightbox.defaultOptions.captionDelay = 250;
